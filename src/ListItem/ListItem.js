fb2085ca45daccaf9b7e5153d54ab42dd9d9cddd
Goran Spasojevic <goky91@gmail.com>
2020-01-04 20:37:01 +0100

    started working on ListItem checkbox functions

--- src/ListItem/ListItem.js
+++ src/ListItem/ListItem.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const ListItem = (props) => {
-    return (
-    <div className="note-body__item">
-        <input name={props.name} type="checkbox"></input>
-        <label htmlFor={props.name}> {props.label} </label>
-        <button>X</button>
-    </div>
-    );
+class ListItem extends Component {
+
+    state = {
+        checked: false
+    }
+
+    checkboxHandler = () => {
+        
+    }
+
+    render() {
+        return (
+            <div className="note-body__item">
+                <input onClick={this.checkboxHandler} name={this.props.name} type="checkbox"></input>
+                <label htmlFor={this.props.name}> {this.props.label} </label>
+                <button>X</button>
+            </div>
+        );
+    }
 };
 
 export default ListItem;
