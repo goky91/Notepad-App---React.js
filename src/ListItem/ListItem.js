8372d39eb89957710c6c0de1aee07bb396750503
Goran Spasojevic <goky91@gmail.com>
2020-01-04 23:26:34 +0100

    Finished creating dynamic list items, still needs deletition funcionallity and some styling.

--- src/ListItem/ListItem.js
+++ src/ListItem/ListItem.js
@@ -3,18 +3,22 @@ import React, {Component} from 'react';
 class ListItem extends Component {
 
     state = {
-        checked: false
+        checked: ''
     }
 
     checkboxHandler = () => {
-        
+        if (this.state.checked === '') {
+            this.setState({checked: 'js-checked'});
+        } else {
+            this.setState({checked: ''});
+        }
     }
 
     render() {
         return (
             <div className="note-body__item">
                 <input onClick={this.checkboxHandler} name={this.props.name} type="checkbox"></input>
-                <label htmlFor={this.props.name}> {this.props.label} </label>
+                <label className={this.state.checked} htmlFor={this.props.name}> {this.props.label} </label>
                 <button>X</button>
             </div>
         );
